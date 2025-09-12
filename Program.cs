3c9f591ebc1df0a332feadcc6a36d9786d7370fb
TheSirLeaf <patrickorb3@gmail.com>
2025-09-12 15:58:36 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//make a simple loop that reads something and writes a mathematical operation
+int readThis = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    readThis = int.TryParse(Console.ReadLine(), out readThis) ? readThis : 0;
+    Console.WriteLine(readThis * readThis);
+}
