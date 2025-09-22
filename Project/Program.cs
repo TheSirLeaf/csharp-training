9ce9672fa434364a23853a554ed6400d8c2eb3e8
TheSirLeaf <patrickorb3@gmail.com>
2025-09-22 00:35:51 -0300

    Experimentando com mais alguns conceitos

--- Project/Program.cs
+++ Project/Program.cs
@@ -2,27 +2,145 @@
 using System;
 using Newtonsoft.Json;
 
-string conteudoArquivo = File.ReadAllText("./Files/venda.json");
-try
-{
-    List<Venda2>? vendas = JsonConvert.DeserializeObject<List<Venda2>>(conteudoArquivo);
 
-    if (vendas != null)
-    {
-        foreach (Venda2 venda in vendas)
-        {
-            Console.WriteLine($"Id: {venda.Id}");
-            Console.WriteLine($"Produto: {venda.Produto}");
-            Console.WriteLine($"Preço: {venda.Preco}");
-            Console.WriteLine($"Data da Venda: {venda.DataVenda}");
-            Console.WriteLine("---------------");
-        }
-    }
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Erro ao ler o arquivo: {ex.Message}");
-}
+
+
+
+
+
+
+
+
+
+
+
+// int numero = 9;
+
+// bool par = numero.EhPar();
+
+// Console.WriteLine($"O número {numero} é {(par ? "par" : "ímpar")}");
+
+
+
+
+
+
+
+
+
+// MeuArray<int> arrayInteiro = new MeuArray<int>();
+// arrayInteiro.adicionarElementoArray(10);
+// arrayInteiro.adicionarElementoArray(20);
+
+// Console.WriteLine(arrayInteiro[0]);
+// Console.WriteLine(arrayInteiro[1]);
+
+// MeuArray<string> arrayString = new MeuArray<string>();
+// arrayString.adicionarElementoArray("Hello");
+// arrayString.adicionarElementoArray("World");
+
+// Console.WriteLine(arrayString[0]);
+// Console.WriteLine(arrayString[1]);
+
+
+
+
+
+
+
+
+// string conteudoArquivo= File.ReadAllText("./Files/venda.json");
+// List<Venda2>? vendas = JsonConvert.DeserializeObject<List<Venda2>>(conteudoArquivo);
+
+// var listaAnonimo = vendas?.Select(x => new { x.Produto, x.Preco });
+
+// foreach (var item in listaAnonimo)
+// {
+//     Console.WriteLine($"Produto: {item.Produto}, Preço: {item.Preco}");
+// }   
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var tipoAnonimo = new {Nome = "Patrick", Sobrenome = "Brito", Altura = 1.8, Idade = 30};
+
+// Console.WriteLine($"Nome: {tipoAnonimo.Nome}");
+// Console.WriteLine($"Sobrenome: {tipoAnonimo.Sobrenome}");
+// Console.WriteLine($"Altura: {tipoAnonimo.Altura}");
+// Console.WriteLine($"Idade: {tipoAnonimo.Idade}");
+// Console.WriteLine($"Tipo: {tipoAnonimo.GetType()}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bool? desejaReceberEmail = null;
+
+// if(desejaReceberEmail.HasValue && desejaReceberEmail.Value)
+// {
+//     Console.WriteLine("O usuário deseja receber email.");
+// }
+// else if(desejaReceberEmail.HasValue && !desejaReceberEmail.Value)
+// {
+//     Console.WriteLine("O usuário não deseja receber email.");
+// }
+// else
+// {
+//     Console.WriteLine("O usuário não informou sua preferência.");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// string conteudoArquivo = File.ReadAllText("./Files/venda.json");
+// try
+// {
+//     List<Venda2>? vendas = JsonConvert.DeserializeObject<List<Venda2>>(conteudoArquivo);
+
+//     if (vendas != null)
+//     {
+//         foreach (Venda2 venda in vendas)
+//         {
+//             Console.WriteLine($"Id: {venda.Id}");
+//             Console.WriteLine($"Produto: {venda.Produto}");
+//             Console.WriteLine($"Preço: {venda.Preco}");
+//             Console.WriteLine($"Data da Venda: {venda.DataVenda}");
+//             Console.WriteLine($"Desconto: {(venda.Desconto.HasValue ? venda.Desconto.Value.ToString() + "%" : "N/A")}");
+//             Console.WriteLine("---------------");
+//         }
+//     }
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine($"Erro ao ler o arquivo: {ex.Message}");
+// }
 
 
 
--- ProjectCommon/Venda2.cs
+++ ProjectCommon/Venda2.cs
@@ -9,10 +9,15 @@ namespace ProjectCommon
     public class Venda2
     {
         public int Id { get; set; }
+
         [JsonProperty("Nome_Produto")]
         public string? Produto { get; set; }
+
         public decimal Preco { get; set; }
+
         public DateTime DataVenda { get; set; }
+        
+        public decimal? Desconto { get; set; }
         
     }
 }
